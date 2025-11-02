25fd6a57329a471a7f82fba3ac456db8e7012ed9
HA QUAN <quan.kieu.2013.dt@gmail.com>
2025-11-02 10:58:08 +0700

    spl test1

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_i2c.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "queue.h"
+#include "timers.h"
+#include "event_groups.h"
+
+#include "user_task.h"
+
+
+typedef struct 
+{
+	GPIO_TypeDef* GPIO_Port;
+	uint16_t GPIO_Pin;
+	GPIOMode_TypeDef GPIO_Mode;
+	GPIOSpeed_TypeDef GPIO_Speed;
+}GPIO_Config_t;
+
+void SystemClock_Config(void);
+void GPIO_Init_Config(GPIO_Config_t* config);
+void GPIO_Config(void);
+
+#endif
---
+++ user_task.c
@@ -0,0 +1,15 @@
+#include "user_task.h"
+
+void Task1(void *pvParameters)
+{
+	(void)*pvParameters;
+	TickType_t LastWakeTime=xTaskGetTickCount();
+	
+	while(1)
+	{
+		LED_GPIO_Port->ODR|=(1<<LED_Pin);
+		vTaskDelayUntil(&LastWakeTime,pdMS_TO_TICKS(200));
+		LED_GPIO_Port->ODR&=~(uint32_t)(1<<LED_Pin);
+		vTaskDelayUntil(&LastWakeTime,pdMS_TO_TICKS(2000));
+	}
+}
