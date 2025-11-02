7798b62bef9064c46883c2924b7d1052db722595
HA QUAN <quan.kieu.2013.dt@gmail.com>
2025-11-02 11:46:28 +0700

    test2

--- user_task.c
+++ user_task.c
@@ -11,5 +11,6 @@ void Task1(void *pvParameters)
 		vTaskDelayUntil(&LastWakeTime,pdMS_TO_TICKS(200));
 		LED_GPIO_Port->ODR&=~(uint32_t)(1<<LED_Pin);
 		vTaskDelayUntil(&LastWakeTime,pdMS_TO_TICKS(2000));
+		//
 	}
 }
